5406a8a85b0d327051b90780872ede115cbd3754
An sejoon <129924048+SeJoon1117@users.noreply.github.com>
2023-06-14 15:05:00 +0900

    Add files via upload
    Signed-off-by: An sejoon <129924048+SeJoon1117@users.noreply.github.com>

---
+++ Chapter06/ch06-Q04.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class MyVector {
+	int* mem;
+	int size;
+public:
+	MyVector(int n = 100, int val = 0) {
+		mem = new int[n];
+		size = n;
+		for (int i = 0; i < size; i++)
+			mem[i] = val;
+	}
+	~MyVector() { delete[] mem; }
+	void show() {
+		for (int i = 0; i < size; i++) {
+			cout << "mem[" << i << "] : " << mem[i] << endl;
+		}
+	}
+};
+
+int main() {
+	MyVector myV(10,10);
+	myV.show();
+}
